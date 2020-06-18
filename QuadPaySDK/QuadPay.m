52ef7f9f27ac2697bfbd016b7b9b60b0a9c19c52
Paul Sauer <paulrsauer@gmail.com>
2020-06-18 17:59:07 -0400

    update prod string

--- QuadPaySDK/QuadPay.m
+++ QuadPaySDK/QuadPay.m
@@ -30,7 +30,7 @@ static QuadPay *__sharedInstance = nil;
 
 - (NSString *)getBaseUrl {
     if ([_environment isEqualToString:@"prod"]) {
-        return @"https://master.gateway.quadpay.xyz/";
+        return @"https://gateway.quadpay.com/";
     }
     if ([_environment isEqualToString:@"sandbox"]) {
         return @"https://sandbox.gateway.quadpay.com/";
