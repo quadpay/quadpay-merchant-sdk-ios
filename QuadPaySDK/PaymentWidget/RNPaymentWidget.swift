d026abc363c32b43d3edb35d2da61ce4e37ebe3f
Petros Andreou <Petros.andreou@zip.co>
2023-08-02 12:58:31 +0300

    Added the fonts in RN wrapper

--- QuadPaySDK/PaymentWidget/RNPaymentWidget.swift
+++ QuadPaySDK/PaymentWidget/RNPaymentWidget.swift
@@ -74,6 +74,45 @@ public final class RNPaymentWidget: UIView {
     
     override public init(frame: CGRect) {
         super.init(frame: .zero)
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskMedium20.ttf",
+            bundle: Bundle.qpResource
+        )
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskMedium25.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskLight20.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskLight25.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskBook20.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskBook25.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskSmBold20.ttf",
+            bundle: Bundle.qpResource
+        )
+        
+        UIFont.jbs_registerFont(
+            withFilenameString: "SharpGroteskSmBold25.ttf",
+            bundle: Bundle.qpResource
+        )
+        
         style()
         layout()
     }
@@ -150,7 +189,7 @@ extension RNPaymentWidget {
                         self.layout()
                     }
                     
-                case .failure(let error):
+                case .failure(_):
                     self.maxFee = 0
                     DispatchQueue.main.async {
                         self.layout()
